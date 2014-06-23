9d15c6832a77581410c363d00ca7a9f90858f330
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-23 14:35:08 -0600

    restore manifest.json and rebase appdirs

--- app/driver.js
+++ app/driver.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	// UI Code here
-	var app = chrome.app.window.create('window.html', {
+	var app = chrome.app.window.create('app/window.html', {
 		id: "dexcomcharting",
 		bounds: {
 			width: 600,

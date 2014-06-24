46c04e5320bbfae37d9794040ca3fd420a777347
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-24 10:01:01 -0600

    save cgm to mongolab

--- app/graphing.js
+++ app/graphing.js
@@ -2,17 +2,42 @@ Promise.all([
 	new Promise(function(resolve) {
 		dexcom.connect().then(function() {
 			setTimeout(function() {
-				dexcom.readFromReceiver(1, resolve);
+				dexcom.readFromReceiver(1, function(d) {
+					resolve(d);
+				});
 			}, 500);
 		});
 	}),
 	new Promise(function(resolve) {
+		console.log("[mongoconfig] Loading");
 		// for loading graphing lib- now embedded
-		resolve();
+		$.ajax({
+			url: "../mongoconfig.json", 
+			success: function(d) {
+				console.log("[mongoconfig] loaded");
+				resolve(JSON.parse(d));
+			}
+		});
 	})
 ]).then(function(results) {
-	var data = results[0];
+	var data = results[0],
+		mongoconfig = results[1];
+	debugger;
+	$.ajax({
+		url: "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=" + mongoconfig.apikey,
+		data: JSON.stringify(data.map(function(plot) {
+			return {
+				device: "dexcom",
+				timestamp: +plot.displayTime,
+				bg: plot.bgValue,
+				direction: plot.trend
+			};
+		})),
+		type: "POST",
+		contentType: "application/json"
+	});
 	var trend = data.map(function(plot) {
+
 		return [
 			+plot.displayTime,
 			plot.bgValue

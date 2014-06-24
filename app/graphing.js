d15b70392b1b068a1b79a4357572d30c3c143fe3
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-24 15:17:50 -0600

    live updates, and dupe info from dex screen

--- app/graphing.js
+++ app/graphing.js
@@ -14,30 +14,35 @@ Number.prototype.toMilliseconds = function() { return this; };
 
 Promise.all([
 	new Promise(function(resolve) {
+		console.debug("[dexcom] loading");
 		dexcom.connect().then(function() {
+			console.debug("[dexcom] loaded");
 			setTimeout(function() {
+				console.debug("[dexcom] reading");
 				dexcom.readFromReceiver(1, function(d) {
+					console.debug("[dexcom] read; disconnecting");
+					dexcom.disconnect();
 					resolve(d);
 				});
 			}, 500);
 		});
 	}),
 	new Promise(function(resolve) {
-		console.log("[mongoconfig] Loading");
+		console.debug("[mongoconfig] Loading");
 		$.ajax({
 			url: "../mongoconfig.json", 
 			success: function(d) {
-				console.log("[mongoconfig] loaded");
+				console.debug("[mongoconfig] loaded");
 				resolve(JSON.parse(d));
 			}
 		});
 	}),
 	new Promise(function(resolve) {
-		console.log("[diypsconfig] Loading");
+		console.debug("[diypsconfig] Loading");
 		$.ajax({
 			url: "../diypsconfig.json", 
 			success: function(d) {
-				console.log("[diypsconfig] loaded");
+				console.debug("[diypsconfig] loaded");
 				resolve(JSON.parse(d));
 			}
 		});
@@ -46,60 +51,99 @@ Promise.all([
 	var data = results[0],
 		mongoconfig = results[1],
 		diypsconfig = results[2];
-	// $.ajax({
-	// 	url: "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=" + mongoconfig.apikey,
-	// 	data: JSON.stringify(data.map(function(plot) {
-	// 		return {
-	// 			device: "dexcom",
-	// 			timestamp: +plot.displayTime,
-	// 			bg: plot.bgValue,
-	// 			direction: plot.trend
-	// 		};
-	// 	})),
-	// 	type: "POST",
-	// 	contentType: "application/json"
-	// });
-	var updateLocalDb = function() {
+	saveToMongoLab = function(data) {
+		// $.ajax({
+		// 	url: "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=" + mongoconfig.apikey,
+		// 	data: JSON.stringify(data.map(function(plot) {
+		// 		return {
+		// 			device: "dexcom",
+		// 			timestamp: +plot.displayTime,
+		// 			bg: plot.bgValue,
+		// 			direction: plot.trend
+		// 		};
+		// 	})),
+		// 	type: "POST",
+		// 	contentType: "application/json"
+		// });	
+	},
+	saveToDiyPS = function(data) {
+		console.debug("[saveToDiyPS] firing off");
+		$.post(diypsconfig.endPoint,
+			{
+				records: JSON.stringify(data.map(function(plot) {
+					return [
+						+plot.displayTime,
+						plot.bgValue,
+						plot.trend
+					];
+				}))
+			}
+		);
+	};
+
+	var lastNewRecord = Date.now();
+	
+	var updateLocalDb = function(data) {
 		chrome.storage.local.get("egvrecords", function(storage) {
 			var existing = storage.egvrecords || [];
 			var max_existing = existing.length > 0?  existing[existing.length - 1].displayTime : 0;
-			var to_save = existing.concat(data.filter(function(egv) {
-				return egv.displayTime > max_existing;
+			var new_records = data.filter(function(egv) {
+				return +egv.displayTime > max_existing;
 			}).map(function(egv) {
 				return {
 					displayTime: +egv.displayTime,
 					bgValue: egv.bgValue,
 					trend: egv.trend
 				};
-			}));
-			chrome.storage.local.set({ egvrecords: to_save }, console.log.bind(console, "Saved results"));
-			$.post(diypsconfig.endPoint,
-				{
-					records: JSON.stringify(data.map(function(plot) {
-						return [
-							+plot.displayTime,
-							plot.bgValue,
-							plot.trend
-						];
-					}))
+			});
+			var to_save = existing.concat(new_records);
+			chrome.storage.local.set({ egvrecords: to_save }, console.debug.bind(console, "[updateLocalDb] Saved results"));
+			console.log("%i new records", new_records.length);
+			if (new_records.length == 0) {
+				if (lastNewRecord + (5).minutes() < Date.now()) {
+					console.log("[updateLocalDb] Something's wrong. We should have new data by now.");
 				}
-			);
+			} else {
+				lastNewRecord = Date.now();
+			}
 		});
 	};
-	updateLocalDb();
+	updateLocalDb(data);
 	setInterval(function() {
-		dexcom.readFromReceiver(1, function(data) {
-			updateLocalDb();
-		});
-	}, (5).minutes());
+		console.log("Attempting to refresh data");
+		try {
+			dexcom.connect().then(function() {
+				setTimeout(function() {
+					dexcom.readFromReceiver(1, function(data) {
+						dexcom.disconnect();
+						updateLocalDb(data);
+					});
+				}, 500);
+			});
+		} catch (e) {
+			console.debug(e);
+		}
+	}, (30).seconds());
 });
 
+var saveToMongoLab = function() {
+	// stub
+}
+
+var saveToDiyPS = function() {
+	// stub
+}
+
 function drawReceiverChart(data) {
+	var t = parseInt($("#timewindow").val(),10);
+	var now = (new Date()).getTime();
 	var trend = data.map(function(plot) {
 		return [
 			+plot.displayTime,
 			plot.bgValue
 		];
+	}).filter(function(plot) {
+		return plot[0] + t.hours() > now;
 	});
 	$.plot(
 		"#dexcomtrend",
@@ -113,15 +157,29 @@ function drawReceiverChart(data) {
 			}
 		}
 	);
+	$("#cgmnow").text(data[data.length - 1].bgValue);
+	$("#cgmdirection").text(data[data.length - 1].trend);
+	$("#cgmtime").text((new Date(data[data.length - 1].displayTime)).toTimeString());
 }
 
+// updated database
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-	debugger;
-	if ("egvrecords" in changes)
+	if ("egvrecords" in changes)  {
 		drawReceiverChart(changes.egvrecords.newValue);
+		saveToMongoLab(changes.egvrecords.newValue);
+		saveToDiyPS(changes.egvrecords.newValue);
+	}
 });
 
+// first load
 chrome.storage.local.get("egvrecords", function(values) {
-	debugger;
 	drawReceiverChart(values.egvrecords);
+});
+
+$(function() {
+	$("#timewindow").change(function(t) {
+		chrome.storage.local.get("egvrecords", function(values) {
+			drawReceiverChart(values.egvrecords);
+		});
+	});
 });

3c0a24fcbbaac15fe7f8e61aaba0b5f7d336a0c1
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-25 11:03:43 -0600

    move number.prototype.* time stuff to own file

--- app/graphing.js
+++ app/graphing.js
@@ -1,17 +1,3 @@
-Number.prototype.milliseconds = function() { return this; };
-Number.prototype.seconds = function() {	return this.milliseconds() * 1000; };
-Number.prototype.minutes = function() { return this.seconds() * 60; };
-Number.prototype.hours = function() { return this.minutes() * 60; };
-Number.prototype.days = function() { return this.hours() * 24; };
-Number.prototype.weeks = function() { return this.days() * 7; };
-Number.prototype.months = function() { return this.days() * 30; };
-
-Number.prototype.toDays = function() { return this.toHours() / 24; };
-Number.prototype.toHours = function() { return this.toMinutes() / 60; };
-Number.prototype.toMinutes = function() { return this.toSeconds() / 60; };
-Number.prototype.toSeconds = function() { return this.toMilliseconds() / 1000; };
-Number.prototype.toMilliseconds = function() { return this; };
-
 Promise.all([
 	new Promise(function(resolve) {
 		console.debug("[dexcom] loading");

67720eb0a20b2f3cbfae02d2235ec5870e139aad
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-26 14:50:42 -0600

    commenting

--- app/datasource/mongolab.js
+++ app/datasource/mongolab.js
@@ -1,17 +1,23 @@
 define(function() {
+	var formatDate = function(d) {
+		return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
+	};
 	return {
 		insert: function(plot, mongoconfig) {
+			// have a unique constraint on date to keep it from inserting too much data.
+			// mongolab returns a 400 when duplicate attempted
 			$.ajax({
 				url: "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=" + mongoconfig.apikey,
-				data: JSON.stringify([{
+				data: JSON.stringify({
 					device: "dexcom",
-					timestamp: +plot.displayTime,
-					bg: plot.bgValue,
+					date: plot.displayTime,
+					dateString: formatDate(new Date(plot.displayTime)),
+					sgv: plot.bgValue,
 					direction: plot.trend
-				}]),
+				}),
 				type: "POST",
 				contentType: "application/json"
 			});
 		}
-	}
+	};
 });

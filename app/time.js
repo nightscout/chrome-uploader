ee067394bc52075f32d795833f55caa7850fdfa8
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-30 10:21:42 -0600

    monkey with global prototype. Only do it if not already done.

--- app/time.js
+++ app/time.js
@@ -1,13 +1,25 @@
-Number.prototype.milliseconds = function() { return this; };
-Number.prototype.seconds = function() {	return this.milliseconds() * 1000; };
-Number.prototype.minutes = function() { return this.seconds() * 60; };
-Number.prototype.hours = function() { return this.minutes() * 60; };
-Number.prototype.days = function() { return this.hours() * 24; };
-Number.prototype.weeks = function() { return this.days() * 7; };
-Number.prototype.months = function() { return this.days() * 30; };
+if (!("milliseconds" in Number.prototype))
+	Number.prototype.milliseconds = function() { return this; };
+if (!("seconds" in Number.prototype))
+	Number.prototype.seconds = function() {	return this.milliseconds() * 1000; };
+if (!("minutes" in Number.prototype))
+	Number.prototype.minutes = function() { return this.seconds() * 60; };
+if (!("hours" in Number.prototype))
+	Number.prototype.hours = function() { return this.minutes() * 60; };
+if (!("days" in Number.prototype))
+	Number.prototype.days = function() { return this.hours() * 24; };
+if (!("weeks" in Number.prototype))
+	Number.prototype.weeks = function() { return this.days() * 7; };
+if (!("months" in Number.prototype))
+	Number.prototype.months = function() { return this.days() * 30; };
 
-Number.prototype.toDays = function() { return this.toHours() / 24; };
-Number.prototype.toHours = function() { return this.toMinutes() / 60; };
-Number.prototype.toMinutes = function() { return this.toSeconds() / 60; };
-Number.prototype.toSeconds = function() { return this.toMilliseconds() / 1000; };
-Number.prototype.toMilliseconds = function() { return this; };
+if (!("toDays" in Number.prototype))
+	Number.prototype.toDays = function() { return this.toHours() / 24; };
+if (!("toHours" in Number.prototype))
+	Number.prototype.toHours = function() { return this.toMinutes() / 60; };
+if (!("toMinutes" in Number.prototype))
+	Number.prototype.toMinutes = function() { return this.toSeconds() / 60; };
+if (!("toSeconds" in Number.prototype))
+	Number.prototype.toSeconds = function() { return this.toMilliseconds() / 1000; };
+if (!("toMilliseconds" in Number.prototype))
+	Number.prototype.toMilliseconds = function() { return this; };

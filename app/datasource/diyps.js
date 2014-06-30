e5dc6bee6e2d09c19474b6b22b6d9e1d0a696812
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-06-30 12:28:35 -0600

    indent a little less (thanks @brianarn)

--- app/datasource/diyps.js
+++ app/datasource/diyps.js
@@ -1,36 +1,34 @@
 define(function() {
-	var config = new Promise(function(loaded) {
-		console.debug("[diyps] Loading config");
-		$.ajax({
-			url: "/diypsconfig.json", 
-			success: function(d) {
-				console.debug("[diyps] loaded config");
-				loaded(JSON.parse(d));
-			}
-		});
+	console.debug("[diyps] Loading config");
+	var config = $.getJSON("/diypsconfig.json", function(d) {
+		console.debug("[diyps] loaded config");
 	});
-	var diyps = {
-		replace: function(data) {
-			config.then(function(diypsconfig) {
-				if (!("endPoint" in diypsconfig || diypsconfig.endPoint.length > 0)) return;
 
-				console.debug("[saveToDiyPS] firing off");
-				$.post(diypsconfig.endPoint, {
-					records: JSON.stringify(data.map(function(plot) {
-						return [
-							+plot.displayTime,
-							plot.bgValue,
-							plot.trend
-						];
-					}))
-				});
-			});
-		}
+	var formatData = function(data) {
+		return JSON.stringify(data.map(function(plot) {
+			return [
+				+plot.displayTime,
+				plot.bgValue,
+				plot.trend
+			];
+		}));
 	};
+
+	var diyps = { };
+	diyps.replace = function(data) { config.then(function(diypsconfig) {
+		if (!("endPoint" in diypsconfig || diypsconfig.endPoint.length > 0)) return;
+
+		console.debug("[saveToDiyPS] firing off");
+		$.post(diypsconfig.endPoint, {
+			records: formatData(data)
+		});
+	}); };
+
 	chrome.storage.onChanged.addListener(function(changes, namespace) {
 		if ("egvrecords" in changes)  {
 			diyps.replace(changes.egvrecords.newValue);
 		}
 	});
+
 	return diyps;
 });
--- app/datasource/mongolab.js
+++ app/datasource/mongolab.js
@@ -1,46 +1,43 @@
 define(function() {
-	var config = new Promise(function(resolve) {
-		console.debug("[mongolab] loading config");
-		$.ajax({
-			url: "/mongoconfig.json", 
-			success: function(d) {
-				console.debug("[mongolab] loaded config");
-				resolve(JSON.parse(d));
-			}
-		});
+	console.debug("[mongolab] loading config");
+	var config = $.getJSON("/mongoconfig.json", function(d) {
+		console.debug("[mongolab] loaded config");
 	});
 	var formatDate = function(d) {
 		return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
 	};
 
-	var mongolab = {
-		insert: function(plot) {
-			// have a unique constraint on date to keep it from inserting too much data.
-			// mongolab returns a 400 when duplicate attempted
-			config.then(function(mongoconfig) {
-				console.log("[mongolab] Writing most recent record to MongoLab");
-				if (!("apiKey" in mongoconfig || mongoconfig.apiKey.length > 0)) return;
-
-				$.ajax({
-					url: "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=" + mongoconfig.apikey,
-					data: JSON.stringify({
-						device: "dexcom",
-						date: plot.displayTime,
-						dateString: formatDate(new Date(plot.displayTime)),
-						sgv: plot.bgValue,
-						direction: plot.trend
-					}),
-					type: "POST",
-					contentType: "application/json"
-				});
-			});
-		}
+	var formatData = function(plot) {
+		return JSON.stringify({
+			device: "dexcom",
+			date: plot.displayTime,
+			dateString: formatDate(new Date(plot.displayTime)),
+			sgv: plot.bgValue,
+			direction: plot.trend
+		});
 	};
+	var mongolabUrl = "https://api.mongolab.com/api/1/databases/dexcomhistory/collections/egv?apiKey=";
+
+	var mongolab = { };
+	mongolab.insert = function(plot) { config.then(function(mongoconfig) {
+		// have a unique constraint on date to keep it from inserting too much data.
+		// mongolab returns a 400 when duplicate attempted
+		
+		console.log("[mongolab] Writing most recent record to MongoLab");
+		if (!("apikey" in mongoconfig || mongoconfig.apikey.length > 0)) return;
+
+		$.ajax({
+			url: mongolabUrl + mongoconfig.apikey,
+			data: formatData(plot),
+			type: "POST",
+			contentType: "application/json"
+		});
+	}); };
 
 	// updated database
 	chrome.storage.onChanged.addListener(function(changes, namespace) {
 		if ("egvrecords" in changes)  {
-			mongolab.insert(changes.egvrecords.newValue[changes.egvrecords.newValue.length - 1])
+			mongolab.insert(changes.egvrecords.newValue[changes.egvrecords.newValue.length - 1]);
 		}
 	});
 

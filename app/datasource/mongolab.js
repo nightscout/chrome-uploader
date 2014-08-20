d1b00280c6c40e6478b9a76ea8c9316c4ffc6c9b
Brian Bosh <bbosh@fortress.health.unm.edu>
2014-08-20 12:35:21 -0600

    new test method for connection details & populate local db from mongolab

--- app/datasource/mongolab.js
+++ app/datasource/mongolab.js
@@ -1,6 +1,32 @@
+// var executionQueue = function() {
+// 	var args = Array.prototype.slice.apply(arguments, 0);
+// 	while (args.length) {
+// 		this.push(args.shift());
+// 	}
+// 	var currFn = queue.shift();
+// 	var queue = this;
+// 	var promiseImpl = function(promise_ok, promise_fail) {
+// 		currFn(function ok() {
+// 			currFn = queue.shift();
+// 			if (typeof currFn == "function") {
+// 				new Promise(promiseImpl);
+// 			} else {
+// 				promise_ok.apply(queue, arguments);
+// 			}
+// 		}, function fail() {
+// 			promise_fail.apply(queue, arguments);
+// 		});
+// 	};
+// 	var myPromise = new Promise(promiseImpl);
+// };
+
+// executionQueue.prototype = [];
+
+
+
 define(function() {
 	var formatDate = function(d) {
-		return d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
+		return (1900 + d.getFullYear()) + "/" + (d.getMonth() + 1) + "/" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes();
 	};
 
 	var formatData = function(plot) {
@@ -37,6 +63,76 @@ define(function() {
 				data: formatData(plot),
 				type: "POST",
 				contentType: "application/json"
+			});
+		});
+	};
+
+	mongolab.populateLocalStorage = function() {
+		(new Promise(function(done) {
+			chrome.storage.local.get("config", function(local) {
+				done(local.config || {});
+			});
+		})).then(function(config) {
+			// have a unique constraint on date to keep it from inserting too much data.
+			// mongolab returns a 400 when duplicate attempted
+
+			console.log("[mongolab] Requesting all data from MongoLab");
+			if (!("mongolab" in config)) return;
+			if (!("apikey" in config.mongolab && config.mongolab.apikey.length > 0)) return;
+			if (!("collection" in config.mongolab && config.mongolab.collection.length > 0)) return;
+			if (!("database" in config.mongolab && config.mongolab.database.length > 0)) return;
+
+			$.getJSON(mongolabUrl + config.mongolab.database + "/collections/" + config.mongolab.collection + "?apiKey=" + config.mongolab.apikey).then(function(data) {
+				chrome.storage.local.get("egvrecords", function(local) {
+					var records = (local.egvrecords || []).concat(data.map(function(record) {
+						return {
+							displayTime: record.date,
+							bgValue: record.sgv,
+							trend: record.direction
+						};
+					}));
+					records.sort(function(a,b) {
+						return a.displayTime - b.displayTime;
+					});
+					records = records.filter(function(rec, ix, all) {
+						if (ix === 0) return true;
+						return all[ix - 1].displayTime != rec.displayTime;
+					});
+
+					chrome.storage.local.set({ egvrecords: records }, console.debug.bind(console, "[mongolab] grabbed all records from interwebs"));
+				});
+			});
+		});
+	};
+
+	mongolab.testConnection = function(apikey, databasename, collectionname) {
+		return new Promise(function(ok, fail) {
+			$.getJSON(mongolabUrl + "?apiKey=" + apikey).then(function(r) {
+				// db ok
+				if (r.filter(function(ml_db_name) {
+					return ml_db_name == databasename;
+				}).length > 0) {
+					$.getJSON(mongolabUrl + databasename +  "/collections?apiKey=" + apikey).then(function(r) {
+						// db ok
+						if (r.filter(function(ml_col_name) {
+							return ml_col_name == collectionname;
+						}).length > 0) {
+							ok();
+						} else {
+							fail({ error: "Bad collection name", type: "collection", avlb: r.filter(function(choice) {
+								return choice.substr(0,7) !== "system.";
+							}), selected: collectionname });
+						}
+					}, function(r) {
+						// db fail
+						fail({ error: "Bad API Key", type: "apikey", avlb: [], selected: apikey });
+					});
+				} else {
+					fail({ error: "Bad database name", type: "database", avlb: r, selected: databasename });
+				}
+			}, function(r) {
+				// db fail
+				fail({ error: "Bad API Key", type: "apikey", avlb: [], selected: apikey });
 			});
 		});
 	};

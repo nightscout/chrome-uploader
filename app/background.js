48f2ad28bb20a2df54697c0428977988a2ee13b2
Brian Bosh <bbosh@Fortress.local>
2014-08-21 10:45:12 -0600

    automatically open chromadex when dexcom plugged in

--- app/background.js
+++ app/background.js
@@ -1,10 +1,51 @@
+var app;
+var isOpen = false;
+
 chrome.app.runtime.onLaunched.addListener(function() {
 	// UI Code here
-	var app = chrome.app.window.create('app/window.html', {
+	chrome.app.window.create('app/window.html', {
 		id: "dexcomcharting",
 		bounds: {
 			width: 800,
 			height: 350
 		}
+	}, function(window) {
+		isOpen = true;
+		app = window;
 	});
 });
+
+function monitorForConnection () {
+	var options = {};
+	options.vendorId = 8867; // 0x22a3
+	options.productId = 71; // 0x0047
+	chrome.usb.getDevices(options, function(devices) {
+		// found == devices.length > 0
+		if (isOpen) {
+			if (devices.length > 0) {
+				// no op
+			} else {
+				// disconnected
+				isOpen = false;
+			}
+		} else {
+			if (devices.length > 0) {
+				// plugged in
+				isOpen = true;
+				chrome.app.window.create('app/window.html', {
+					id: "dexcomcharting",
+					bounds: {
+						width: 800,
+						height: 350
+					}
+				}, function(w) {
+					app = w;
+				});
+			} else {
+				// no op
+			}
+		}
+	});
+}
+
+setInterval(monitorForConnection, 1000);

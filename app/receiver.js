060f954fca898c8796eaf1e322626e974a71f5b7
Brian Bosh <github@alternateinterior.com>
2014-11-14 13:12:55 -0700

    support mmol on graph again

--- app/receiver.js
+++ app/receiver.js
@@ -1,5 +1,4 @@
 require(["./bloodsugar"], function(convertBg) {
-
 	var jqShow = $.fn.show;
 	$.fn.show = function(domid) {
 		var o = jqShow.apply(this, [domid]);
@@ -8,9 +7,6 @@ require(["./bloodsugar"], function(convertBg) {
 		}
 		return o;
 	}
-
-
-
 
 	var currData = null;
 	$("#dexcomtrend").bind("plothover", function (event, pos, item) {
@@ -27,54 +23,45 @@ require(["./bloodsugar"], function(convertBg) {
 		}
 	});
 
-
 	function hover(x, y, contents) {
+		//Adrian: Had to use workaround with container as maximum opacity is inherited from parent.
+		//Make text opaque and background transparent.
+		var hovercontainer = jQuery('<div id="hover"></div>').css({
+			position: 'absolute',
+			top: y + 8,
+			left: x + 8
+		});
 
+		var hovertext = jQuery('<div id="hovertext">' + contents + '</div>').css( {
+			position: 'relative',
+			top: 0,
+			left: 0,
+	 		width: '100%',
+			height: '100%',
+			padding: '2px',
+			'text-align': 'center',
+			'font-size': '120%',
+			'z-index': 2,
+			opacity: 1
+		});
 
-	//Adrian: Had to use workaround with container as maximum opacity is inherited from parent.
-	//Make text opaque and background transparent.
-
-	var hovercontainer = jQuery('<div id="hover"></div>').css( {
-        	position: 'absolute',
-        	top: y + 8,
-       		left: x + 8
-    	});
-
-	var hovertext = jQuery('<div id="hovertext">' + contents + '</div>').css( {
-        	position: 'relative',
-        	top: 0,
-       		left: 0,
- 		width: '100%',
-       		height: '100%',
-		padding: '2px',
-		'text-align': 'center',
-		'font-size': '120%',
-		'z-index': 2,
-        	opacity: 1
-    	});
-
-	var hoverbg = jQuery('<div id="hoverbg"></div>').css( {
-        	'background-color': '#fff',
-        	position: 'absolute',
-        	top: 0,
-       		left: 0,
-       		width: '100%',
-       		height: '100%',
-		border: '2px outset #fff',
-		'z-index': 1,
-        	opacity: .75
-    	});
-	hovercontainer.append(hovertext);
-	hovercontainer.append(hoverbg);
-	hovercontainer.appendTo("body").fadeIn(200);
+		var hoverbg = jQuery('<div id="hoverbg"></div>').css( {
+			'background-color': '#fff',
+			position: 'absolute',
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			border: '2px outset #fff',
+			'z-index': 1,
+			opacity: .75
+		});
+		hovercontainer.append(hovertext);
+		hovercontainer.append(hoverbg);
+		hovercontainer.appendTo("body").fadeIn(200);
 	}
 
-
-
-
-
 	function drawReceiverChart(data) {
-
 		var low, high, time;
 		Promise.all([
 			new Promise(function(ready) {
@@ -87,104 +74,126 @@ require(["./bloodsugar"], function(convertBg) {
 						high = 130;
 					}
 
-
 					if ("trenddisplaytime" in values) {
 						time = values.trenddisplaytime || 12;
 					} else {
 						time = 12;
 					}
 
-	
 					ready();
 				});
 			})
 		]).then(function(o){
-		var t = time; //parseInt($("#timewindow").val(),10); //Adrian: was: var t = 12;
-		var now = (new Date()).getTime();
-		var trend = data.map(function(plot) {
-			return [
-				+plot.displayTime,
-				convertBg(plot.bgValue)
-			];
-		}).filter(function(plot) {
-			return plot[0] + t.hours() > now;
-		});
+			var t = time; //parseInt($("#timewindow").val(),10); //Adrian: was: var t = 12;
+			var now = (new Date()).getTime();
+			var trend = data.map(function(plot) {
+				return [
+					+plot.displayTime,
+					convertBg(plot.bgValue)
+				];
+			}).filter(function(plot) {
+				return plot[0] + t.hours() > now;
+			});
 
-		var trendIn = trend.filter(function(plot){
-			return plot[1]<=high && plot[1]>=low;
-		});
-		var trendHigh = trend.filter(function(plot){
-			return plot[1]>high;
-		});
-		var trendLow = trend.filter(function(plot){
-			return plot[1]<low;
-		
-		});
+			var trendIn = trend.filter(function(plot){
+				return plot[1]<=high && plot[1]>=low;
+			});
+			var trendHigh = trend.filter(function(plot){
+				return plot[1]>high;
+			});
+			var trendLow = trend.filter(function(plot){
+				return plot[1]<low;
+			
+			});
 
-		var ticksz = [1, "hour"];
-		var timeformat = "%H:%M"
-		if (t<3){
-			ticksz = [15, "minute"];
-		}
-		if (t>24){
-			ticksz = [1, "day"];
-			timeformat = "%m/%d"
-		}
-
-		$.plot(
-			"#dexcomtrend",
-			[{
-				       
-       			data: trendIn,
-        		color: "#00FF00",
-        		points: { show: true },
-        		lines: { show: false }
-			},
-			{
-			data: trendHigh,
-			color: "#FFFF00",
-        		points: { show: true, fill: true, fillColor: "#FFDD00" },
-        		lines: { show: false }
-			},
-			{
-       			data: trendLow,
-        		color: "#FF0000",
-        		points: { show: true },
-        		lines: { show: false }
-			}],
-			{
-				xaxis: {
-					mode: "time",
-					timezone: "browser",
-					timeformat: timeformat,
-					minTickSize: ticksz,
-					tickColor: "#555",
-				}, 
-				yaxis: {
-					min: 0,
-					max: 350,
-					tickColor: "#555",
-				},
-				grid: {
-    					markings: [
-      					{ color: '#FF0000', lineWidth: 2, yaxis: { from: low, to: low } },
-					{ color: '#FFFF00', lineWidth: 2, yaxis: { from: high, to: high } },
-   					 ],
-					hoverable: true
-  				}
+			var ticksz = [1, "hour"];
+			var timeformat = "%H:%M"
+			if (t<3){
+				ticksz = [15, "minute"];
 			}
-		);
-		if (data.length) {
-			$("#cgmnow").text(convertBg(data[data.length - 1].bgValue));
-			$("#cgmdirection").text(data[data.length - 1].trend);
-			$("#cgmtime").text((new Date(data[data.length - 1].displayTime)).format("h:ia"));
-		}
+			if (t>24){
+				ticksz = [1, "day"];
+				timeformat = "%m/%d"
+			}
 
-
-
+			$.plot(
+				"#dexcomtrend",
+				[
+					{
+						data: trendIn,
+						color: "#00FF00",
+						points: {
+							show: true
+						},
+						lines: {
+							show: false
+						}
+					},
+					{
+						data: trendHigh,
+						color: "#FFFF00",
+						points: {
+							show: true,
+							fill: true,
+							fillColor: "#FFDD00"
+						},
+						lines: {
+							show: false
+						}
+					},
+					{
+						data: trendLow,
+						color: "#FF0000",
+						points: {
+							show: true
+						},
+						lines: {
+							show: false
+						}
+					}
+				],
+				{
+					xaxis: {
+						mode: "time",
+						timezone: "browser",
+						timeformat: timeformat,
+						minTickSize: ticksz,
+						tickColor: "#555",
+					}, 
+					yaxis: {
+						min: 0,
+						max: convertBg(400),
+						tickColor: "#555",
+					},
+					grid: {
+						markings: [
+		  					{
+		  						color: '#FF0000',
+		  						lineWidth: 2,
+		  						yaxis: {
+		  							from: convertBg(low),
+		  							to: convertBg(low)
+		  						}
+		  					},
+							{
+								color: '#FFFF00',
+								lineWidth: 2,
+								yaxis: {
+									from: convertBg(high),
+									to: convertBg(high)
+								}
+							}
+						],
+						hoverable: true
+					}
+				}
+			);
+			if (data.length) {
+				$("#cgmnow").text(convertBg(data[data.length - 1].bgValue));
+				$("#cgmdirection").text(data[data.length - 1].trend);
+				$("#cgmtime").text((new Date(data[data.length - 1].displayTime)).format("h:ia"));
+			}
 		});
-
-
 	}
 
 	// updated database

d919b5071bbd839ab65611009b68170a2dc10c95
AdrianLxM <adrian.m@inbox.com>
2014-11-26 11:49:10 +0100

    added a1c estimation to distribution report

--- app/report/glucosedistribution.js
+++ app/report/glucosedistribution.js
@@ -12,9 +12,10 @@ function generate_report(data, high, low) {
 		$("<th>Range</th>").appendTo(thead);
 		$("<th>% of Readings</th>").appendTo(thead);
 		$("<th># of Readings</th>").appendTo(thead);
-		$("<th>Mean</th>").appendTo(thead);
+		$("<th><>Mean</th>").appendTo(thead);
 		$("<th>Median</th>").appendTo(thead);
 		$("<th>SD</th>").appendTo(thead);
+		$("<th>A1c estimation*</th>").appendTo(thead);
 		thead.appendTo(table);
 
 		["Low", "Normal", "High"].forEach(function(range) {
@@ -38,22 +39,41 @@ function generate_report(data, high, low) {
 			var midpoint = Math.floor(rangeRecords.length / 2);
 			//var statistics = ss.(new Statician(rangeRecords.map(function(r) { return r.localBg; }))).stats;
 
-			$("<td>" + range + "</td>").appendTo(tr);
+			$("<td><strong>" + range + ": </strong></td>").appendTo(tr);
 			$("<td>" + Math.floor(100 * rangeRecords.length / data.length) + "%</td>").appendTo(tr);
 			$("<td>" + rangeRecords.length + "</td>").appendTo(tr);
 			if (rangeRecords.length > 0) {
 				$("<td>" + Math.floor(10*Statician.mean(localBgs))/10 + "</td>").appendTo(tr);
 				$("<td>" + rangeRecords[midpoint].localBg + "</td>").appendTo(tr);
 				$("<td>" + Math.floor(Statician.standard_deviation(localBgs)*10)/10 + "</td>").appendTo(tr);
+				$("<td> </td>").appendTo(tr);
 			} else {
 				$("<td>N/A</td>").appendTo(tr);
 				$("<td>N/A</td>").appendTo(tr);
 				$("<td>N/A</td>").appendTo(tr);
+				$("<td> </td>").appendTo(tr);
 			}
 
 			table.append(tr);
 		});
-
+		
+		var tr = $("<tr>");
+		$("<td><strong>Overall: </strong></td>").appendTo(tr);
+		$("<td> </td>").appendTo(tr);
+		$("<td>" + data.length + "</td>").appendTo(tr);
+		if (data.length > 0) {
+			var localBgs = data.map(function(r) { return r.localBg; });
+			$("<td>" + Math.round(10*ss.mean(localBgs))/10 + "</td>").appendTo(tr);
+			$("<td>" + Math.round(10*ss.quantile(localBgs, 0.5))/10+ "</td>").appendTo(tr);
+			$("<td>" + Math.round(ss.standard_deviation(localBgs)*10)/10 + "</td>").appendTo(tr);
+			$("<td><center>" + Math.round(10*(ss.mean(localBgs)+46.7)/28.7)/10 + "%</center></td>").appendTo(tr);
+		} else {
+			$("<td>N/A</td>").appendTo(tr);
+			$("<td>N/A</td>").appendTo(tr);
+			$("<td>N/A</td>").appendTo(tr);
+			$("<td>N/A</td>").appendTo(tr);
+		}
+		table.append(tr);
 		report.append(table);
 
 		setTimeout(function() {

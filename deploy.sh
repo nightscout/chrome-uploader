1b8fc236b03e0c3e503f68ea2fcef7fd73a42a1e
Brian Bosh <github@alternateinterior.com>
2014-12-09 09:39:39 -0700

    deploy script. COPY SRC FOLDER FIRST, AND RUN IN THE COPY. eg: cp -r chrome-uploader distributable; cd distributable; ./deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm -rf node_modules
+rm -rf .git
+rm npm-debug.log
+rm -rf tests
+rm resource/chrome-promotional-small.png
+rm vendor/jquery/dist/jquery.js
+rm vendor/jquery/dist/jquery.min.map
+rm -rf vendor/jquery/src
+rm vendor/jquery-ui/jquery-ui.js
+rm -rf vendor/jquery-ui/ui
+mv vendor/jquery-ui/themes/smoothness/ .smoothness
+rm -rf vendor/jquery-ui/themes/*
+mv .smoothness vendor/jquery-ui/themes/smoothness
+rm vendor/jquery-ui/themes/smoothness/jquery-ui.css
+rm -rf vendor/qunit
+rm -rf vendor/qunit-phantom-runner
+rm -rf vendor/bootstrap/test-infra
+rm Brocfile.js

add27e21c592ebae4e8de41cb8e32fb056c622f4
Brian Bosh <github@alternateinterior.com>
2014-12-17 19:27:45 -0700

    make success report work with big datasets

--- app/report/success.js
+++ app/report/success.js
@@ -1,39 +1,16 @@
-Promise.all([
-	new Promise(function(ready) {
-		chrome.storage.local.get("egvrecords", function(values) {
-			ready(values.egvrecords);
-		});
-	}),
-	new Promise(function(ready) {
-		require(["../bloodsugar"], function(convertBg) {
-			setTimeout(function() {
-				ready(convertBg);
-			}, 1 /* give it a second to configure itself */);
-		});	
-	}),
-	new Promise(function(ready) {
-		chrome.storage.local.get("config", function(values) {
-			ready(values.config.targetrange || {
-				low: 70,
-				high: 180
-			});
-		});
-	})
-]).then(function(o) {
-	var data = o[0],
-		convertBg = o[1],
-		range = o[2];
-	var low = range.low,
-		high = range.high;
+require(["../egv_records", "../bloodsugar", "/app/config.js!"], function(data, convertBg, config) {
+	debugger;
+	var low = parseInt(config.targetrange.low),
+		high = parseInt(config.targetrange.high);
 	var config = {
 		low: convertBg(low),
 		high: convertBg(high)
 	};
 	var now = Date.now();
 	var period = (7).days();
-	var firstDataPoint = Math.min.apply(Math, data.map(function(record) {
-		return record.displayTime;
-	}));
+	var firstDataPoint = data.reduce(function(min, record) {
+		return Math.min(min, record.displayTime);
+	}, Number.MAX_VALUE);
 	if (firstDataPoint < 1390000000000) firstDataPoint = 1390000000000;
 	var quarters = Math.floor((Date.now() - firstDataPoint) / period);
 
@@ -67,55 +44,59 @@ Promise.all([
 		upperQuartile: 0,
 		average: 0
 	};
-	quarters = dim(quarters).map(function(blank, n) {
-		var starting = new Date(now - (n+1) * period),
-			ending = new Date(now - n * period);
-		return {
-			starting: starting,
-			ending: ending,
-			records: data.filter(function(record) {
-				return record.displayTime > starting &&
-				record.displayTime <= ending &&
-				"bgValue" in record &&
-				 /\d+/.test(record.bgValue.toString());
-			})
-		};
-	}).filter(function(quarter) {
-		return quarter.records.length > 0;
-	}).map(function(quarter, ix, all) {
-		var bgValues = quarter.records.map(function(record) {
-			return parseInt(record.bgValue,10);
+	try {
+		quarters = dim(quarters).map(function(blank, n) {
+			var starting = new Date(now - (n+1) * period),
+				ending = new Date(now - n * period);
+			return {
+				starting: starting,
+				ending: ending,
+				records: data.filter(function(record) {
+					return record.displayTime > starting &&
+					record.displayTime <= ending &&
+					"bgValue" in record &&
+					 /\d+/.test(record.bgValue.toString());
+				})
+			};
+		}).filter(function(quarter) {
+			return quarter.records.length > 0;
+		}).map(function(quarter, ix, all) {
+			var bgValues = quarter.records.map(function(record) {
+				return parseInt(record.bgValue,10);
+			});
+			quarter.standardDeviation = ss.standard_deviation(bgValues);
+			quarter.average = bgValues.length > 0? (sum(bgValues) / bgValues.length): "N/A";
+			quarter.lowerQuartile = ss.quantile(bgValues, 0.25); 
+			quarter.upperQuartile = ss.quantile(bgValues, 0.75);
+			quarter.numberLow = bgValues.filter(function(bg) {
+				return bg < low;
+			}).length;
+			quarter.numberHigh = bgValues.filter(function(bg) {
+				return bg >= high;
+			}).length;
+			quarter.numberInRange = bgValues.length - (quarter.numberHigh + quarter.numberLow);
+
+			quarter.percentLow = (quarter.numberLow / bgValues.length) * 100;
+			quarter.percentInRange = (quarter.numberInRange / bgValues.length) * 100;
+			quarter.percentHigh = (quarter.numberHigh / bgValues.length) * 100;
+
+			averages.percentLow += quarter.percentLow / all.length;
+			averages.percentInRange += quarter.percentInRange / all.length;
+			averages.percentHigh += quarter.percentHigh / all.length;
+			averages.lowerQuartile += quarter.lowerQuartile / all.length;
+			averages.upperQuartile += quarter.upperQuartile / all.length;
+			averages.average += quarter.average / all.length;
+			averages.standardDeviation += quarter.standardDeviation / all.length;
+			return quarter;
 		});
-		quarter.standardDeviation = ss.standard_deviation(bgValues);
-		quarter.average = bgValues.length > 0? (sum(bgValues) / bgValues.length): "N/A";
-		quarter.lowerQuartile = ss.quantile(bgValues, 0.25); 
-		quarter.upperQuartile = ss.quantile(bgValues, 0.75);
-		quarter.numberLow = bgValues.filter(function(bg) {
-			return bg < config.low;
-		}).length;
-		quarter.numberHigh = bgValues.filter(function(bg) {
-			return bg >= config.high;
-		}).length;
-		quarter.numberInRange = bgValues.length - (quarter.numberHigh + quarter.numberLow);
-
-		quarter.percentLow = (quarter.numberLow / bgValues.length) * 100;
-		quarter.percentInRange = (quarter.numberInRange / bgValues.length) * 100;
-		quarter.percentHigh = (quarter.numberHigh / bgValues.length) * 100;
-
-		averages.percentLow += quarter.percentLow / all.length;
-		averages.percentInRange += quarter.percentInRange / all.length;
-		averages.percentHigh += quarter.percentHigh / all.length;
-		averages.lowerQuartile += quarter.lowerQuartile / all.length;
-		averages.upperQuartile += quarter.upperQuartile / all.length;
-		averages.average += quarter.average / all.length;
-		averages.standardDeviation += quarter.standardDeviation / all.length;
-		return quarter;
-	});
+	} catch (e) {
+		console.log(e);
+	}
 
 	var lowComparison = function(quarter, averages, field, invert) {
-		if (quarter[field] < averages[field]) {
+		if (quarter[field] < averages[field] * 0.8) {
 			return (invert? "bad": "good");
-		} else if (quarter[field] > averages[field]) {
+		} else if (quarter[field] > averages[field] * 1.2) {
 			return (invert? "good": "bad");
 		} else {
 			return "";
@@ -123,7 +104,7 @@ Promise.all([
 	}
 
 	var lowQuartileEvaluation = function(quarter, averages) {
-		if (quarter.lowerQuartile < range.low) {
+		if (quarter.lowerQuartile < low) {
 			return "bad";
 		} else {
 			return lowComparison(quarter, averages, "lowerQuartile");
@@ -131,7 +112,7 @@ Promise.all([
 	}
 
 	var upperQuartileEvaluation = function(quarter, averages) {
-		if (quarter.upperQuartile > range.high) {
+		if (quarter.upperQuartile > high) {
 			return "bad";
 		} else {
 			return lowComparison(quarter, averages, "upperQuartile");
@@ -139,9 +120,9 @@ Promise.all([
 	}
 
 	var averageEvaluation = function(quarter, averages) {
-		if (quarter.average > range.high) {
+		if (quarter.average > high) {
 			return "bad";
-		} else if (quarter.average < range.low) {
+		} else if (quarter.average < low) {
 			return "bad";
 		} else {
 			return lowComparison(quarter, averages, "average", true);
